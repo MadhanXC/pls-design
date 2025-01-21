881a9bd333b6e2ca692eaedf58413d74a68670b6
MadhanXC <91551365+MadhanXC@users.noreply.github.com>
2025-01-20 22:44:39 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pls-design/",
+  // base: "/pls-design/",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
